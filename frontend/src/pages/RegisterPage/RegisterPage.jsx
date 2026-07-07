10b9d54e8524597ae12fded54003f0d8627f63e5
William <nhannk15@gmail.com>
2026-07-07 09:59:26 +0700

    feat: added a Login Page and a Register Page

---
+++ frontend/src/pages/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,10 @@
+import SignUpForm from "../../components/RegisterForm/SignUpForm";
+import "./RegisterPage.css";
+
+export default function RegisterPage() {
+    return (
+        <div className="register-page">
+            <SignUpForm />
+        </div>
+    );
+}
