fab3cfbcda5c4f4c352cdfc7771eb2f8f9db90ee
nauppnouv <tranvuongquan2707@gmail.com>
2026-07-12 14:03:08 +0700

    feat: add Dockerfile, Nginx config, and Vite setup for containerization

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': 'http://localhost:8080',
+      '/auth': 'http://localhost:8080',
+    }
+  }
 })
